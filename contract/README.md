bb7fe19ece9fb964711ed2103ce916a82ba0ff6c
yusakapon <>
2023-02-25 15:54:51 +0900

    [Fix] depoy and. verify setting

--- contract/README.md
+++ contract/README.md
@@ -9,5 +9,8 @@ npx hardhat help
 npx hardhat test
 REPORT_GAS=true npx hardhat test
 npx hardhat node
-npx hardhat run scripts/deploy.ts
+npx hardhat run scripts/deploy.ts --network astar
+npx hardhat verify --network astar 0x000..
 ```
+
+https://blockscout.com/astar/address/0x6FBe5b8ddef07fdDeC2F5A8A29136abD73D155f1
--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -13,6 +13,10 @@ const config: HardhatUserConfig = {
       url: process.env.SHIBUYA_API_URL,
       accounts: [process.env.PRIVATE_KEY ?? ""],
     },
+    mumbai: {
+      url: process.env.MUMBAI_API_URL,
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    },
     hardhat: {
       forking: {
         url: process.env.ASTAR_API_URL ?? "",
@@ -21,6 +25,24 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: process.env.ASTAR_SCAN_API_KEY ?? "",
+    customChains: [
+      {
+        network: "astar",
+        chainId: 592,
+        urls: {
+          apiURL: "https://blockscout.com/astar/api",
+          browserURL: "https://blockscout.com/astar",
+        },
+      },
+      {
+        network: "shibuya",
+        chainId: 81,
+        urls: {
+          apiURL: "https://blockscout.com/shibuya/api",
+          browserURL: "https://blockscout.com/shibuya",
+        },
+      },
+    ],
   },
 };
 
